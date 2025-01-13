77d023e9e62be8453dbf4cc7381494e3d5f5e558
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-13 17:07:27 +0330

    Update task.entity.ts

--- server/src/task/entities/task.entity.ts
+++ server/src/task/entities/task.entity.ts
@@ -1,6 +1,41 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'tasks' })
 export class TaskEntity {
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    comment: 'The label of the task',
+  })
+  label: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['TODO', 'DOING', 'DONE'],
+    default: 'TODO',
+    nullable: false,
+    comment: 'The current state of the task',
+  })
+  state: 'TODO' | 'DOING' | 'DONE';
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    nullable: false,
+    comment: 'The creation date of the task',
+  })
+  createdAt: string;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    nullable: false,
+    comment: 'The last update date of the task',
+  })
+  updatedAt: string;
 }
