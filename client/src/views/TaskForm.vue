d71f7c7589443bff1b24a5ad52bbff4ea9986c7f
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-13 20:57:40 +0330

    add task

---
+++ client/src/views/TaskForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const addTask = () => {}
+</script>
+<template>
+  <form class="row g-3">
+    <div class="col-12">
+      <input type="password" class="form-control" placeholder="Enter your task title" />
+    </div>
+    <div class="col-12">
+      <textarea class="form-control" placeholder="Enter your task description" :rows="5" />
+    </div>
+    <div class="col-12">
+      <select class="form-control">
+        <option value="TODO">Todo</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
+    </div>
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary mb-3" @click="addTask">Add task</button>
+    </div>
+  </form>
+</template>
---
+++ client/src/views/TaskList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="ss">
+    <div class="d-grid gap-4">
+      <div class="card" v-for="item in 14" :key="item">
+        <div class="card-body">
+          <h4 class="">Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, molestiae.</p>
+          <div class="btn btn-success">Done</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.ss {
+  overflow-y: auto;
+}
+</style>
